48c76403bf8940c1d55e438966a08b4a8f2f496d
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-12-18 00:04:49 -0500

    Fix positioning of layout components relative to container.

--- src/apps/LayoutBuilder/index.tsx
+++ src/apps/LayoutBuilder/index.tsx
@@ -1,9 +1,28 @@
-import { createSignal, onCleanup, onMount } from 'solid-js';
+import {
+  createEffect,
+  createSignal,
+  mergeProps,
+  onCleanup,
+  onMount,
+} from 'solid-js';
+
+const COMPONENT_WIDTH = 96;
+const SPACING = 24;
+const CONTAINER_PADDING = 20;
 
 const LayoutBuilder = () => {
   const [displayRef, setDisplayRef] = createSignal<HTMLDivElement>();
+  const [compContainerRef, setCompContainerRef] =
+    createSignal<HTMLDivElement>();
 
   const [displayBounds, setDisplayBounds] = createSignal({
+    x: 0,
+    y: 0,
+    height: 0,
+    width: 0,
+  });
+
+  const [compContainerBounds, setCompContainerBounds] = createSignal({
     x: 0,
     y: 0,
     height: 0,
@@ -15,7 +34,17 @@ const LayoutBuilder = () => {
       const bounds = displayRef()!.getBoundingClientRect();
       setDisplayBounds({
         x: bounds.left,
-        y: bounds.y,
+        y: bounds.top,
+        height: bounds.height,
+        width: bounds.height,
+      });
+    }
+
+    if (compContainerRef()) {
+      const bounds = compContainerRef()!.getBoundingClientRect();
+      setCompContainerBounds({
+        x: bounds.left,
+        y: bounds.top,
         height: bounds.height,
         width: bounds.height,
       });
@@ -38,9 +67,28 @@ const LayoutBuilder = () => {
         <div ref={setDisplayRef} class="bg-white w-full h-full"></div>
       </div>
       {/* Components Box */}
-      <div class="w-full bg-dark-5 h-xl -mb-44 p-5 flex flex-wrap gap-4 content-start">
-        <LayoutComponent name="Row" color="pink" />
-        <LayoutComponent name="Column" color="blue" />
+      <div
+        ref={setCompContainerRef}
+        class="w-full bg-dark-5 h-xl -mb-44 p-5 flex flex-wrap gap-4 content-start"
+      >
+        <LayoutComponent
+          index={0}
+          name="Row"
+          color="pink"
+          position={{
+            x: compContainerBounds().x,
+            y: compContainerBounds().y,
+          }}
+        />
+        <LayoutComponent
+          index={1}
+          name="Column"
+          color="blue"
+          position={{
+            x: compContainerBounds().x,
+            y: compContainerBounds().y,
+          }}
+        />
       </div>
     </div>
   );
@@ -49,11 +97,18 @@ const LayoutBuilder = () => {
 export default LayoutBuilder;
 
 interface LayoutComponentProps {
+  index: number;
   name: string;
   color?: string;
+  position?: { x: number; y: number };
 }
 
 const LayoutComponent = (props: LayoutComponentProps) => {
+  props = mergeProps({ color: 'white' }, props);
+
+  const xOffset = props.index * (SPACING + COMPONENT_WIDTH) + CONTAINER_PADDING;
+  const yOffset = CONTAINER_PADDING;
+
   const [compRef, setCompRef] = createSignal<HTMLDivElement>();
   const [isDragging, setIsDragging] = createSignal(false);
 
@@ -62,7 +117,7 @@ const LayoutComponent = (props: LayoutComponentProps) => {
     { equals: false }
   );
   const [position, setPosition] = createSignal(
-    { x: 0, y: 0 },
+    props.position ?? { x: 0, y: 0 },
     { equals: false }
   );
 
@@ -71,8 +126,8 @@ const LayoutComponent = (props: LayoutComponentProps) => {
       const compBounds = compRef()!.getBoundingClientRect();
       setIsDragging(true);
       setStartPosition({
-        x: e.clientX - compBounds.left,
-        y: e.clientY - compBounds.top,
+        x: e.clientX - compBounds.left + (props.position?.x ?? 0) + xOffset,
+        y: e.clientY - compBounds.top + (props.position?.y ?? 0) + yOffset,
       });
     }
   };
@@ -92,6 +147,13 @@ const LayoutComponent = (props: LayoutComponentProps) => {
     }
   };
 
+  createEffect(() => {
+    if (props.position && compRef()) {
+      compRef()!.style.top = (props.position?.y ?? 0) + yOffset + 'px';
+      compRef()!.style.left = (props.position?.x ?? 0) + xOffset + 'px';
+    }
+  });
+
   onMount(() => {
     document.addEventListener('mousemove', onMouseMove);
     document.addEventListener('mouseup', onMouseUp);
@@ -105,7 +167,7 @@ const LayoutComponent = (props: LayoutComponentProps) => {
   return (
     <div
       ref={setCompRef}
-      class={`flex items-center justify-center w-24 h-10 bg-transparent border-${props.color}-4 border-1 rounded-sm lines-gradient to-${props.color}-4/50 cursor-move fixed top-0 left-0`}
+      class={`flex items-center justify-center w-24 h-10 bg-${props.color}/30 border-${props.color}-4 border-1 rounded-sm lines-gradient to-${props.color}-4/50 cursor-move fixed select-none`}
       onMouseDown={onMouseDown}
       style={{ transform: `translate(${position().x}px, ${position().y}px)` }}
     >
