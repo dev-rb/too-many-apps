ffa17ff4fac921d20a9360f36445bbcfefbfb954
dev-rb <43100342+dev-rb@users.noreply.github.com>
2022-12-24 15:19:29 -0500

    Update styling for tree view, html view, and preview pane.

--- src/apps/LayoutBuilder/Preview.tsx
+++ src/apps/LayoutBuilder/Preview.tsx
@@ -15,23 +15,21 @@ const Preview = () => {
         <h1 class="text-lg color-dark-2"> Preview </h1>
       </div>
       <div class="w-full h-[1px] border-t-dark-3 border-t-1 mt-2" />
-      <div class="flex mt-2 w-full">
+      <div
+        class="flex mt-2 w-full bg-dark-4 rounded-r-sm rounded-l-sm relative after:(content-empty bg-blue-7 rounded-sm absolute top-0 h-full w-50% transition-transform)"
+        classList={{
+          ['after:(translate-x-0)']: activeView() === 0,
+          ['after:(translate-x-full)']: activeView() === 1,
+        }}
+      >
         <button
-          class="appearance-none border-none outline-none p-2 color-white cursor-pointer w-full rounded-sm"
-          classList={{
-            ['bg-blue-7 hover:bg-blue-6']: activeView() === 0,
-            ['bg-transparent']: activeView() !== 0,
-          }}
+          class="appearance-none border-none bg-transparent outline-none p-2 color-white cursor-pointer w-full rounded-sm transition-colors z-2"
           onClick={() => setActiveView(0)}
         >
           Tree
         </button>
         <button
-          class="appearance-none border-none outline-none p-2 color-white cursor-pointer w-full rounded-sm"
-          classList={{
-            ['bg-blue-7 hover:bg-blue-6']: activeView() === 1,
-            ['bg-transparent']: activeView() !== 1,
-          }}
+          class="appearance-none border-none bg-transparent outline-none p-2 color-white cursor-pointer w-full rounded-sm transition-colors z-2"
           onClick={() => setActiveView(1)}
         >
           HTML
@@ -39,11 +37,11 @@ const Preview = () => {
       </div>
       <div class="custom-v-scrollbar flex flex-col mt-4 overflow-auto pr-2 select-none h-full">
         <Show when={activeView() === 1}>
-          <p class="text-sm color-dark-2"> {'<div id="app">'} </p>
+          <p class="text-sm color-dark-1"> {'<div id="app">'} </p>
         </Show>
         <Index each={Object.values(builder.componentState.components).filter((v) => v.parent === undefined)}>
           {(component) => (
-            <div class="flex flex-col">
+            <div class="flex flex-col " classList={{ ['ml-4']: activeView() === 1 }}>
               <Dynamic
                 component={views[activeView()]()}
                 allLayers={builder.componentState.components}
@@ -58,7 +56,7 @@ const Preview = () => {
           )}
         </Index>
         <Show when={activeView() === 1}>
-          <p class="text-sm color-dark-2"> {'</div>'} </p>
+          <p class="text-sm color-dark-1"> {'</div>'} </p>
         </Show>
       </div>
     </div>
@@ -82,7 +80,7 @@ const TreeView = (props: TreeViewProps) => {
   const builder = useBuilderContext();
   const isComponentActive = createSelector(() => builder.componentState.selected);
 
-  const depthMargin = 10 * props.depth;
+  const depthMargin = 12 * props.depth;
 
   return (
     <div
@@ -98,7 +96,7 @@ const TreeView = (props: TreeViewProps) => {
         classList={{
           ['bg-blue-7 hover:bg-blue-6']: isComponentActive(props.id),
           ['bg-dark-4 hover:bg-dark-4']: !isComponentActive(props.id),
-          ['before:(content-empty h-50% absolute -left-2 top-0 w-[1px] border-l-dark-2 border-l-1) after:(content-empty w-[8px] absolute top-50% -translate-y-50% -left-2 h-[1px] border-t-dark-2 border-t-1)']:
+          ['after:(content-empty w-[8px] absolute top-50% -translate-y-50% -left-2 h-[1px] border-t-dark-2 border-t-1)']:
             props.depth > 0,
         }}
         onClick={() => props.selectLayer(props.id)}
@@ -148,11 +146,11 @@ const HtmlView = (props: TreeViewProps) => {
       >
         <p class="hover:bg-dark-4" onClick={() => props.selectLayer(props.id)}>
           <span class="color-red-5">&lt;div </span>{' '}
-          <span class="color-indigo-3">
-            id=<span class="color-green-3">"{props.id}"</span>
+          <span class="color-purple-3">
+            id=<span class="color-green-2">"{props.id}"</span>
           </span>{' '}
-          <span class="color-indigo-3">
-            class=<span class="color-green-3">"{props.name}"</span>
+          <span class="color-purple-3">
+            class=<span class="color-green-2">"{props.name}"</span>
           </span>
           <span class="color-red-5">&gt;</span>
           {/* {`<div id="${props.id}" class="${props.name}">`} */}
