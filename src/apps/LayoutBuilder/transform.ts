41339413b6c1363fef610d6c7cc0df8148a473e6
dev-rb <43100342+dev-rb@users.noreply.github.com>
2023-02-05 01:17:39 -0500

    Add missing file from last commit.

---
+++ src/apps/LayoutBuilder/transform.ts
@@ -0,0 +1,37 @@
+import { e } from 'unocss';
+import { Bounds, Size, XYPosition } from '~/types';
+import { clamp } from '~/utils/math';
+import { calculateDistances } from './snapping';
+
+export const getRelativeTransformedBounds = (
+  parentBounds: Bounds & Size,
+  initialParentBounds: Bounds & Size,
+  elementBounds: Bounds & Size,
+  isFlippedX: boolean,
+  isFlippedY: boolean
+) => {
+  const nx =
+    (isFlippedX ? initialParentBounds.right - elementBounds.right : elementBounds.left - initialParentBounds.left) /
+    initialParentBounds.width;
+  const ny =
+    (isFlippedY ? initialParentBounds.bottom - elementBounds.bottom : elementBounds.top - initialParentBounds.top) /
+    initialParentBounds.height;
+
+  const nw = elementBounds.width / initialParentBounds.width;
+  const nh = elementBounds.height / initialParentBounds.height;
+
+  let newX = parentBounds.left + parentBounds.width * nx;
+  let newY = parentBounds.top + parentBounds.height * ny;
+
+  const width = parentBounds.width * nw;
+  const height = parentBounds.height * nh;
+
+  return {
+    left: newX,
+    top: newY,
+    right: newX + width,
+    bottom: newY + height,
+    width,
+    height,
+  };
+};
